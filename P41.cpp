bb6f1cfc452a94c86a2d80da8df4f11e9e39c300
LuigiOdinson <116271810+LuigiOdinson@users.noreply.github.com>
2022-11-01 11:15:13 +0330

    Add files via upload

---
+++ P41.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//Romina Mianji 
+
+int main() {
+	float x,y;
+	float j,t,z,b;
+	
+	cin>>x>>y;
+	j=x+y;
+	t=x-y;
+	z=x*y;
+	b=(int)x%(int)y;
+
+	cout<<"jam:"<<j<<"\n";
+	cout<<"tafrig:"<<t<<"\n";
+	cout<<"zarb:"<<z<<"\n";
+	cout<<"bagimande:"<<b<<"\n";
+}
