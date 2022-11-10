66a06a8cd77ffb31787391ee7d92bceae620f623
LuigiOdinson <116271810+LuigiOdinson@users.noreply.github.com>
2022-11-10 10:53:49 +0330

    Add files via upload

---
+++ P52.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+ main() {
+	char character;
+	cin>>character;
+	switch (character){
+		case 'a': case'e': case'i':
+		case 'o': case'u' : case'y':
+		case 'A': case'E': case'I':
+		case 'O': case'U' : case'Y':
+			cout<<"VOWEL";
+	}
+}
+
