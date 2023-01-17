85ee3c36a82b0f59de85492ccd3b8b9434596ad5
RominaM <116271810+LuigiOdinson@users.noreply.github.com>
2023-01-17 15:44:48 +0330

    Update P81.cpp

--- P8/P81.cpp
+++ P8/P81.cpp
@@ -2,16 +2,20 @@
 using namespace std;
 
 main() {
-	int nums[10],i,j,max,min,sum;
+	int nums[10],i,max,min,sum;
 	double M;
 	max=nums[0];
 	min=nums[10];
 	
 	for(i=0;i<10;i++){
 		cin>>nums[i];
+	}
+	for(i=0;i<10;i++){
 		if(nums[i]>max){
 			max=nums[i];
 		}
+	}
+	for(i=10;i>0;i--){
 		if(nums[i]<min){
 			min=nums[i];
 		}
@@ -20,4 +24,3 @@ main() {
 	M=sum/2.0;
 	cout<<endl<<M;
 }
-
