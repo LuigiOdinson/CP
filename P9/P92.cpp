0d417b598c1733d699723f5342c643416293fd87
RominaM <116271810+LuigiOdinson@users.noreply.github.com>
2023-01-17 12:22:20 +0330

    Update P92.cpp

--- P9/P92.cpp
+++ P9/P92.cpp
@@ -8,15 +8,22 @@ main() {
 	for(i=0;i<10;i++){
 		cin>>nums[i];
 	}
-	min1 = nums[0];
-	min2 = nums[0];
+	if(nums[0]<nums[1]){
+		min1 = nums[0];
+		min2 = nums[1];	
+	}
+	else{
+		min1 = nums[1];
+		min2 = nums[0];
+	}
 	for(i=0;i<10;i++){
 		if(nums[i]<min1){
 			min2=min1;
 			min1=nums[i];
 		}
+		if(nums[i]>min1 && nums[i]<min2){
+			min2=nums[i];
+		}
 	}
 	cout<<"second smallest number is "<<min2; 
 }
-
-
